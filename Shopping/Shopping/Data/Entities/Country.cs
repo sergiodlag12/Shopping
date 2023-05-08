0440e70173e13bb7479a3ca91af0745c27a1f5b7
Sergio David Latorre Guevara <sergio.latorre-guevara@siemens-energy.com>
2023-05-08 08:29:51 -0500

    Se creo la BD y se hizo CRUD en la entidad Country

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name="País")]
+        [MaxLength(50, ErrorMessage ="El campo {0} deb tener máximo {1} caracteres.")]
+        [Required(ErrorMessage ="Elcampo {0} es obligatorio.")]
+        public string Name { get; set; }
+    }
+}
